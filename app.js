a54810cbb2d029fb27b5f80cf37473d5f5cdf336
VivekChatterjee <vivuchat84@gmail.com>
2021-06-29 14:56:01 +0530

    My first react website

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require("express");
+
+const ejs = require("ejs");
+
+const bodyparser = require("body-parser");
+
+const app = express();
+ var items = ["Buy Food","Cook Food", "Eat Food"];
+
+app.set('view engine', 'ejs');
+
+app.use(bodyparser.urlencoded({extended:true}));
+
+app.use(express.static("public"));
+
+app.get("/", function (req, res) {
+
+    var date = new Date();
+    var day = "";
+
+    var options = {
+        weekday : "long",
+        day : "numeric",
+        month : "long"
+    };
+ 
+    var day = date.toLocaleDateString("en-US", options);
+
+    res.render("list", {theDay :day, newListItems : items});
+});
+
+app.post("/", function(req,res){
+    var item = req.body.newItem;
+    items.push(item);
+    
+    res.redirect("/");
+});
+
+
+app.listen(3000, function () {
+    console.log("Server is starting on port 3000");
+});
